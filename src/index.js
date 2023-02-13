a7920d4600a22821381bd4b8771d81842723b87c
Uliana-Redfeather <111862073+Uliana-Redfeather@users.noreply.github.com>
2023-02-13 15:40:09 +0700

    init: finish task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morse = '';
+    for (i=0;i<expr.length;i+=10){
+        if (expr.substring(i,i+10)=='**********'){
+            morse = morse + ' '
+        } 
+        else {
+            let morseletter = ''
+        for (j=0;j<10;j+=2){
+            let tempstr = expr.substring(i+j,i+j+2)
+            
+            if (tempstr=="00"){
+
+            } if (tempstr=="10"){
+                morseletter = morseletter + "."
+            } if (tempstr=="11")
+            {
+                morseletter = morseletter + "-"
+            }
+            
+            }
+            morse = morse + MORSE_TABLE[morseletter] 
+        }
+  
+    
+} 
+return morse
+
 }
 
 module.exports = {
     decode
 }
+
